f124392986d0df3f6d7f553d8b9a7ec655476c7e
Catherine Kwak <khopsickle@users.noreply.github.com>
2017-01-06 10:39:19 -0700

    Grid rendering and creating complete!

--- script.js
+++ script.js
@@ -1,32 +1,65 @@
 var Model = {
   grid: [],
-  // gen list of values (1,2)
-  gridSetup: function(size){
-  
+  cardsArray: [],
+  // score
 
-     
+  gridSetup: function(size){
+    this.grid = [];
+    this.generateCards(size);
+
+    for(var col = 1; col <= size; col++){
+      var column = [];
+      this.grid.push(column);
+
+      for(var cell = 1; cell <= size; cell++){
+        var index = Math.floor(Math.random() * this.cardsArray.length);
+        var element = this.cardsArray.splice(index, 1)[0];
+        column.push(element);
+      }
+    }
   },
 
   generateCards: function(size){
-    var valueArray = []
+    for(var i = 1; i <= (size*size / 2); i++){
+      this.cardsArray.push(i);
+      this.cardsArray.push(i);
+    }
+  },
 
-    var values = function(size){
-      for(var i = 1; i <= size; i++){
-        valueArray.push(i);
-        valueArray.push(i); 
-      }
-    }  
-  }
+  getCardValue: function(cell,col) {
+    return this.grid[col][cell];
+  },
 
-}
+  // getGridSize: function() {
+  //   return this.grid.length;
+  // },
+};
 
 var View = {
+  $container: $('.container'),
 
-}
+  renderGrid: function(size) {
+    for(var col = 0; col < size; col++){
+      for (var cell = 0; cell < size; cell++) {
+        var $card = $('<span class="card">');
+        this.$container.append($card);
+        $card.attr({'data-x': col, 'data-y': cell});
+      }
+      this.$container.append('<br>');
+    }
+  },
+};
 
 var Controller = {
+  init: function(size) {
+    Model.gridSetup(size);
+    View.renderGrid(size);
+  },
+};
 
-}
+$(document).ready(function() {
+  Controller.init(4);
+});
 
 /*
   2x2 grid (eventually a bigger even numbered grid)
@@ -34,7 +67,7 @@ var Controller = {
     [[1,2],[2,1]]
   ON click get index number (via div?)
   Reveal picture/number at index
-  When they click on another div 
+  When they click on another div
   (we'll distinguish by setting a class "flipped" on click, and content will be revealed)
   Reveal that card
   check to see if cards are a match (via values stored in Model array)
